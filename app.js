12fe6e1339533b951e06df45a3a92468177041c3
mimmo888 <166280800+mimmo888@users.noreply.github.com>
2024-04-15 13:48:15 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// create a javascript file and html file
+// link app.js to index.html
+// create variables for the combination
+
+//create a dialog box welcoming the chosen one
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+
+// create the variables that result in the code
+const combo1 = 100/10
+const combo2 = 27+13
+const combo3 = 56-17
+
+// final variable including finished combo
+const finalCombo = combo1 + " - " + combo2 + " - " + combo3;
+
+// combine message with code to create the final output
+alert(finalCombo + " " + " " + "...don't let anyone know you have it, or else.")
